e46516f0dc3e50105315bf645d7d166224b647bd
khang-uit <khang94lbt@gamil.com>
2021-11-13 01:57:40 +0700

    make it better

--- index.js
+++ index.js
@@ -17,8 +17,8 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 
 
-app.use(express.json({ extended: true }));
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({  limit: '30mb', extended: true }));
+app.use(express.urlencoded({  limit: '30mb', extended: true }));
 
 
 app.use('/posts', postRoutes);
